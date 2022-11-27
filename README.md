3230db4ec2843767ce73b691aa2189059dba08c7
steve brazier <steve@meadsteve.dev>
2022-11-27 16:30:30 +0100

    clear out project junk and start from scratch

--- README.md
+++
@@ -1,7 +0,0 @@
-# Advent of code 2022
-
-Usage:
-
-```
-npm run solve -- -d 1
-```
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,5 @@
-export default {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  preset: 'ts-jest/presets/default-esm',
-  transform: {
-    '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    'src/**/*.mts',
-    '!src/**/*.d.ts',
-    '!src/**/*.d.mts',
-  ],
 };
