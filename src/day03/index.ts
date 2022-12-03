0d7703bdbd83b9d2a6fccac6951157922677346f
steve brazier <steve@meadsteve.dev>
2022-12-03 09:44:16 +0100

    refactoring ready for part 2

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,6 +1,9 @@
 import { readLines } from '../fileHelpers';
 import { aMap, sum } from '../asyncHelpers';
 
+type ListOfItems = string;
+type Item = string;
+
 export async function solvePartOne() {
   const lines = readLines('src/day03/input.txt');
   return (await totalPriority(lines)).toString();
@@ -11,20 +14,24 @@ export async function solvePartTwo() {
   return 'TODO';
 }
 
-export function splitPack(input: string): [string, string] {
+export function splitPack(input: ListOfItems): [ListOfItems, ListOfItems] {
   return [input.slice(0, input.length / 2), input.slice(input.length / 2)];
 }
 
-export function findCommonItem([left, right]: [string, string]): string {
-  for (let i = 0; i < left.length; i++) {
-    if (right.includes(left[i])) {
-      return left[i];
+export function findCommonItem([first, ...rest]: ListOfItems[]): Item {
+  for (let i = 0; i < first.length; i++) {
+    if (isInAllLists(first[i], rest)) {
+      return first[i];
     }
   }
   throw new Error('No common item found');
 }
 
-export function getPriority(input: string): number {
+function isInAllLists(item: Item, lists: ListOfItems[]): boolean {
+  return lists.every((list) => list.includes(item));
+}
+
+export function getPriority(input: Item): number {
   const charCode = input.charCodeAt(0);
   if (charCode > 97) {
     return charCode - 96;
@@ -33,7 +40,7 @@ export function getPriority(input: string): number {
 }
 
 export async function totalPriority(
-  bags: AsyncGenerator<string>,
+  bags: AsyncGenerator<ListOfItems>,
 ): Promise<number> {
   const compartmentPairs = aMap(bags, splitPack);
   const commonItems = aMap(compartmentPairs, findCommonItem);
