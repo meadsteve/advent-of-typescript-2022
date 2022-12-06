f24c4dd6d2128a50fd78b0057c41b3d69f184456
steve brazier <steve@meadsteve.dev>
2022-12-06 20:22:20 +0100

    day 6 part 2

--- __tests__/day06/index.test.ts
+++ __tests__/day06/index.test.ts
@@ -1,12 +1,34 @@
-import { findMarkerPosition, solvePartOne } from '../../src/day06';
+import {
+  findStartOfMessageMarkerPosition,
+  findStartOfPacketMarkerPosition,
+  solvePartOne,
+  solvePartTwo,
+} from '../../src/day06';
 
 describe('day 6', () => {
-  it('can find the location of the marker position', async function () {
-    expect(await findMarkerPosition('bvwbjplbgvbhsrlpgdmjqwftvncz')).toEqual(5);
-    expect(await findMarkerPosition('nppdvjthqldpwncqszvftbrmjlhg')).toEqual(6);
+  it('can find the location of the start of packet marker position', async function () {
+    expect(
+      await findStartOfPacketMarkerPosition('bvwbjplbgvbhsrlpgdmjqwftvncz'),
+    ).toEqual(5);
+    expect(
+      await findStartOfPacketMarkerPosition('nppdvjthqldpwncqszvftbrmjlhg'),
+    ).toEqual(6);
+  });
+
+  it('can find the location of the start of message marker position', async function () {
+    expect(
+      await findStartOfMessageMarkerPosition('bvwbjplbgvbhsrlpgdmjqwftvncz'),
+    ).toEqual(23);
+    expect(
+      await findStartOfMessageMarkerPosition('nppdvjthqldpwncqszvftbrmjlhg'),
+    ).toEqual(23);
   });
 
   it('can solve part one', async function () {
     expect(await solvePartOne()).toEqual('1598');
+  });
+
+  it('can solve part two', async function () {
+    expect(await solvePartTwo()).toEqual('2414');
   });
 });
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -4,21 +4,34 @@ import { readLines } from '../fileHelpers';
 export async function solvePartOne() {
   const line: string = (await (await readLines('src/day06/input.txt')).next())
     .value;
-  const markerPosition = await findMarkerPosition(line);
+  const markerPosition = await findStartOfPacketMarkerPosition(line);
   return markerPosition.toString();
 }
 
 export async function solvePartTwo() {
-  const lines = readLines('src/day06/input.txt');
-  return 'todo';
+  const line: string = (await (await readLines('src/day06/input.txt')).next())
+    .value;
+  const markerPosition = await findStartOfMessageMarkerPosition(line);
+  return markerPosition.toString();
 }
 
-export async function findMarkerPosition(input: string) {
+export async function findStartOfPacketMarkerPosition(input: string) {
   const letterWindow = slidingWindow(toAsyncGenerator(input.split('')), 4);
   for await (const [position, letters] of enumerate(letterWindow)) {
     const uniqueLetters = new Set(letters);
     if (uniqueLetters.size === 4) {
       return position + 4;
+    }
+  }
+  throw new Error("Couldn't find marker position");
+}
+
+export async function findStartOfMessageMarkerPosition(input: string) {
+  const letterWindow = slidingWindow(toAsyncGenerator(input.split('')), 14);
+  for await (const [position, letters] of enumerate(letterWindow)) {
+    const uniqueLetters = new Set(letters);
+    if (uniqueLetters.size === 14) {
+      return position + 14;
     }
   }
   throw new Error("Couldn't find marker position");
