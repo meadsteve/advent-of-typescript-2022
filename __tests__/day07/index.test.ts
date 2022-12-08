74a5cd3be316d1d9fbb54199924fe77587316e1f
steve brazier <steve@meadsteve.dev>
2022-12-08 19:06:30 +0100

    create code to parse the input for day 7

--- __tests__/day07/index.test.ts
+++ __tests__/day07/index.test.ts
@@ -1,4 +1,36 @@
 import { Directory, File } from '../../src/day07';
+import { fromAsyncGenerator, toAsyncGenerator } from '../../src/asyncHelpers';
+
+type Command = { command: 'ls' } | { command: 'cd'; args: string };
+type CommandAndResult = [Command, string[]];
+
+async function* parseInput(
+  input: AsyncGenerator<string>,
+): AsyncGenerator<CommandAndResult> {
+  let command: Command = parseCommand((await input.next()).value);
+  let result: string[] = [];
+  for await (const line of input) {
+    if (line[0] === '$') {
+      yield [command, result];
+      result = [];
+      command = parseCommand(line);
+    } else {
+      result.push(line);
+    }
+  }
+  yield [command, result];
+}
+
+function parseCommand(command: string): Command {
+  const [_dollar, commandName, ...args] = command.split(' ');
+  if (commandName === 'ls') {
+    return { command: 'ls' };
+  } else if (commandName === 'cd') {
+    return { command: 'cd', args: args.join(' ') };
+  } else {
+    throw new Error(`Unknown command ${commandName}`);
+  }
+}
 
 describe('day 7', () => {
   describe('The file system', () => {
@@ -49,6 +81,28 @@ describe('day 7', () => {
         .createDirectory('b')
         .addFile(new File('file_4.txt', 50));
       expect(root.size).toEqual(100);
+    });
+  });
+  describe('the command input parser', () => {
+    it('should break the input into command and result pairs', async function () {
+      const input = toAsyncGenerator([
+        '$ cd /',
+        '$ ls',
+        'dir a',
+        '14848514 b.txt',
+        '8504156 c.dat',
+        'dir d',
+        '$ cd a',
+      ]);
+      const commandAndResultPairs = await fromAsyncGenerator(parseInput(input));
+      expect(commandAndResultPairs).toEqual([
+        [{ command: 'cd', args: '/' }, []],
+        [
+          { command: 'ls' },
+          ['dir a', '14848514 b.txt', '8504156 c.dat', 'dir d'],
+        ],
+        [{ command: 'cd', args: 'a' }, []],
+      ]);
     });
   });
 });
